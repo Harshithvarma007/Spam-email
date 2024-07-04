fe1d84dba5e08ec8f20f6eb91843a42341bebc8f
Harshithvarma007 <112501476+Harshithvarma007@users.noreply.github.com>
2024-07-04 19:19:11 +0530

    Updated utils

--- src/Spam_mail/utils/common.py
+++ src/Spam_mail/utils/common.py
@@ -4,14 +4,12 @@ import yaml
 from Spam_mail import logger
 import json
 import joblib
-from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
 
 
 
-@ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     """reads yaml file and returns
 
@@ -37,7 +35,6 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     
 
 
-@ensure_annotations
 def create_directories(path_to_directories: list, verbose=True):
     """create list of directories
 
@@ -51,7 +48,6 @@ def create_directories(path_to_directories: list, verbose=True):
             logger.info(f"created directory at: {path}")
 
 
-@ensure_annotations
 def save_json(path: Path, data: dict):
     """save json data
 
@@ -67,7 +63,6 @@ def save_json(path: Path, data: dict):
 
 
 
-@ensure_annotations
 def load_json(path: Path) -> ConfigBox:
     """load json files data
 
@@ -84,7 +79,6 @@ def load_json(path: Path) -> ConfigBox:
     return ConfigBox(content)
 
 
-@ensure_annotations
 def save_bin(data: Any, path: Path):
     """save binary file
 
@@ -96,7 +90,6 @@ def save_bin(data: Any, path: Path):
     logger.info(f"binary file saved at: {path}")
 
 
-@ensure_annotations
 def load_bin(path: Path) -> Any:
     """load binary data
 
@@ -112,7 +105,6 @@ def load_bin(path: Path) -> Any:
 
 
 
-@ensure_annotations
 def get_size(path: Path) -> str:
     """get size in KB
 
